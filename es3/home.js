b9cf52ebf5e47b27de027f8c513ed625dd0fd350
Nicolò Battaglia <gaara0633@gmail.com>
2020-03-30 10:32:14 +0200

    fix

--- es3/home.js
+++ es3/home.js
@@ -1,8 +1,15 @@
 //Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
-var numero1 = parseInt(prompt("Inserisci il primo numero"));
-var numero2 = parseInt(prompt("Inserisci il secondo numero"));
-var numero3 = parseInt(prompt("Inserisci il terzo numero"));
-var numero4 = parseInt(prompt("Inserisci il quarto numero"));
-var numero5 = parseInt(prompt("Inserisci il quinto numero"));
-
-document.getElementById('mio_id').innerHTML = numero1 + numero2 + numero3 + numero4 + numero5;
+var somma = 0;
+for (var i = 0; i < 5; i++) {
+  var numUtente = parseInt(prompt("Inserisci 5 numeri"));
+  var somma = somma + numUtente;
+  document.getElementById('mio_id').innerHTML = "La somma dei tuoi numeri è = " + somma;
+}
+//var i = 0;
+//var somma = 0;
+//while (i < 5) {
+  //var numUtente = parseInt(prompt("Inserisci 5 numeri"));
+  //var somma = somma + numUtente;
+  //i++;
+  //document.getElementById('mio_id').innerHTML = "La somma dei tuoi numeri è = " + somma;
+//}
